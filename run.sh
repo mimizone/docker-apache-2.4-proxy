2122f41145344747605b4d71e72d843942ede0cc
Dominic Price <dominic.price@nottingham.ac.uk>
2016-10-12 12:57:43 +0100

    Apache runtime directories created in run.sh /var/lock/apache2 and /var/run/apache2 are not created correctly so the run script now creates them.

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 chown www-data:www-data /www -R
+mkdir -p /var/lock/apache2
+mkdir -p /var/run/apache2
 source /etc/apache2/envvars
 tail -F /var/log/apache2/* &
 exec apache2 -D FOREGROUND
